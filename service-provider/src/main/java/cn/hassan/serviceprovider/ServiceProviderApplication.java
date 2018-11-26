f595444acbd7b53cf1439a4e1d1227e6a9c80cd3
hassan <hassanbox@qq.com>
2018-11-26 21:17:05 +0800

    eureka

--- service-provider/src/main/java/cn/hassan/serviceprovider/ServiceProviderApplication.java
+++ service-provider/src/main/java/cn/hassan/serviceprovider/ServiceProviderApplication.java
@@ -3,9 +3,11 @@ package cn.hassan.serviceprovider;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @MapperScan("cn.hassan.**.mapper")
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceProviderApplication {
 
     public static void main(String[] args) {
