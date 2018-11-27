ce31367323b1fd6a17c0cdda5bc7ce318d8725f9
hassan <hassanbox@qq.com>
2018-11-27 21:54:37 +0800

    eureka

---
+++ service-eureka-02/src/main/java/cn/hassan/serviceeureka02/ServiceEureka02Application.java
@@ -0,0 +1,14 @@
+package cn.hassan.serviceeureka02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceEureka02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEureka02Application.class, args);
+    }
+}
